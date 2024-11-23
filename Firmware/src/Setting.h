e846e763719eb4998792aea4c100384ed7cb15da
Ha Nguyen <ha.nguyenduc2002@email.com>
2024-11-23 09:04:26 +0700

    fix bug

--- Firmware/src/Setting.h
+++ Firmware/src/Setting.h
@@ -16,10 +16,4 @@ enum KEYSTATUS
 /// Số lượng pedal/nút bấm tối đa
 #define MAX_BUTTONS 4 
 
-const uint button_pins[MAX_BUTTONS] = {PIN_PEDAL01, PIN_PEDAL02, PIN_PEDAL03, PIN_PEDAL04};
-#define uint button_prevalues[MAX_BUTTONS];   /// Giá trị trước đó, ở dạng digital 0/1 của nút bấm
-#define uint button_curvalues[MAX_BUTTONS];   /// Giá trị hiện thời, ở dạng digital 0/1 của nút bấm
-#define KEYSTATUS button_status[MAX_BUTTONS]; /// Trạng thái hiện thời của các nút bấm
 
-/// @brief Thời điểm của lần lặp trước đó
-#define unsigned long TimeOfPreLoop;
--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -2,6 +2,13 @@
 #include <BleKeyboard.h>
 #include <Setting.h>
 
+const uint button_pins[MAX_BUTTONS] = {PIN_PEDAL01, PIN_PEDAL02, PIN_PEDAL03, PIN_PEDAL04};
+uint button_prevalues[MAX_BUTTONS];   /// Giá trị trước đó, ở dạng digital 0/1 của nút bấm
+uint button_curvalues[MAX_BUTTONS];   /// Giá trị hiện thời, ở dạng digital 0/1 của nút bấm
+KEYSTATUS button_status[MAX_BUTTONS]; /// Trạng thái hiện thời của các nút bấm
+
+/// @brief Thời điểm của lần lặp trước đó
+unsigned long TimeOfPreLoop;
 #define DEFAUL_BLENAME "Foot Keyboard"
 FootKeyboardBuilder footKeyboardBuilder(DEFAUL_BLENAME, "NDT Device Manufacturer", 100);
 
