e99b84a7f5c7fe6a84bfa50c0cac172ba1c50041
Ha Nguyen <ha.nguyenduc2002@email.com>
2024-12-17 10:36:45 +0700

    Update WebUI

---
+++ Software/webui/src/app/component/scanDevice/index.jsx
@@ -0,0 +1,7 @@
+export default function ScanDeviceButton() {
+    return (
+      <div className="scan-device-container">
+        <button className="scan-device-button">Scan device</button>
+      </div>
+    );
+  }
